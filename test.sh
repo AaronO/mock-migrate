13131290b3fd215b273fe110a33a216e58ae7b6b
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-08-04 00:25:48 -0700

    Add test.sh

---
+++ test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# Launch static server
+python -m SimpleHTTPServer 5566 &
+
+# Keep PID
+PID=$!
+echo "PID is ${PID}"
+
+# Wait for server to be up
+sleep 2
+
+# Clean out folder
+rm -rf ./out/*
+
+# Make tmp folder
+mkdir -p /tmp/out_tmp
+
+# Download and extract tar to tmp folder
+curl "http://localhost:5566/in.tgz" 2>/dev/null | tar -C /tmp/out_tmp -zxv
+
+# Migrate files
+mv /tmp/out_tmp/home ./out/home
+
+# Kill server
+kill -s KILL ${PID}
+
+# Cleanup tmp folder
+rm -rf /tmp/out_tmp
+
